125324b0aaebe37e7efb12c7f2abbddd65e60c89
yang <yangchao905589518@126.com>
2017-10-18 23:43:26 +0800

    淘淘商城

---
+++ taotao-manager/taotao-manager-Service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.taotao.service;
+
+/**根据商品id查询商品信息Test类
+ * Created by Administrator on 2017/10/18.
+ */
+public interface ItemService {
+
+
+}
---
+++ taotao-manager/taotao-manager-Service/src/main/java/com/taotao/service/ItemServiceImpl.java
@@ -0,0 +1,7 @@
+package com.taotao.service;
+
+/**
+ * Created by Administrator on 2017/10/18.
+ */
+public class ItemServiceImpl {
+}
